6afa28018714629a205986f9555fc766f1c8b1a0
Janith Niroshan <129001487+neroIJN@users.noreply.github.com>
2024-10-22 20:13:56 +0530

    done

--- Client/travel_app/app/layout.tsx
+++ Client/travel_app/app/layout.tsx
@@ -1,33 +1,28 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import Navbar from "../components/Navbar";
-import Footer from "@/components/Footer";
+import type { Metadata } from 'next';
 
-
-
+import './globals.css'
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 export const metadata: Metadata = {
-  title: "Travel",
-  description: "Travel WEB with NEXT JS",
-};
+  title: 'Travel',
+  description: 'Travel UI/UX App for Camping',
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <body>
         <Navbar />
-        
         <main className="relative overflow-hidden">
           {children}
         </main>
-      
-        <Footer/>
-
+        <Footer />
       </body>
     </html>
-  );
+  )
 }
--- Client/travel_app/components/Navbar.tsx
+++ Client/travel_app/components/Navbar.tsx
@@ -3,44 +3,39 @@ import Image from "next/image"
 import Link from "next/link"
 import Button from "./Button"
 
-
 const Navbar = () => {
-    return (
-        <nav className="  flexBetween max-container padding-container relative z-30 py-5">
-            <Link href={"/"}>
-                <Image src="/hilink-logo.svg" alt="logo" width={80} height={30} />
+  return (
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
+      <Link href="/">
+        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+      </Link>
 
-            </Link>
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
+            {link.label}
+          </Link>
+        ))}
+      </ul>
 
-            <ul className="hidden h-full gap-12 lg:flex">
-                {NAV_LINKS.map(link => (
-                    <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-extrabold" >
-                        {link.label}
-                    </Link>
-                ))}
+      <div className="lg:flexCenter hidden">
+        <Button 
+          type="button"
+          title="Login"
+          icon="/user.svg"
+          variant="btn_dark_green"
+        />
+      </div>
 
-            </ul>
-
-            <div className="lg:flexCenter hidden">
-                <Button
-                    type="button"
-                    title="Login"
-                    icon="user.svg"
-                    variant="btn_dark_green"
-                />
-            </div>
-
-            <Image 
-             src = "menu.svg"
-             alt= "menu"
-             width={30}
-             height={30}
-            />
-
-            
-
-        </nav>
-    )
+      <Image 
+        src="menu.svg"
+        alt="menu"
+        width={32}
+        height={32}
+        className="inline-block cursor-pointer lg:hidden"
+      />
+    </nav>
+  )
 }
 
 export default Navbar
